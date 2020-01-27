60ab1883476d92c9a4bcce58bdf1d8e3e762b556
Aung Zin Phyo <aungzinphyo94@gmail.com>
2020-01-28 00:02:51 +0630

    Add Title Row

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,53 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //#docregion titleSection
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        children:[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500]
+                  ),
+                )
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+    //#enddoregion titleSection
+
     return MaterialApp(
-      title: 'Flutter Layout',
+      title: 'Flutter layout',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Flutter layout'),
         ),
-        body: Center(
-          child: Text('Hello World'),
+        body: Column(
+          children: <Widget>[
+            titleSection
+          ],
         ),
       ),
     );
